93115c38d069dd73793ed15049ca10303e7744b5
nrbnayon <nrbnayon@gmail.com>
2025-05-07 13:13:41 +0600

    add GlobalError and NotFound components, update metadata, and include favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,10 +8,24 @@ import { AuthProvider } from "@/contexts/AuthContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Lukhish - Internal Dialogue",
-  description: "A platform for internal dialogue and avatar management",
-  generator: "v0.dev",
+export const metadata = {
+  title:
+    "Internal dialogue: Your AI-Powered internal dialogue and avatar management for Efficient Communication",
+  description:
+    "Internal dialogue is your AI-powered ai assistant that streamlines communication, organizes your stress, and boosts productivity!",
+  keywords:
+    "AI Avatar assistant, Internal dialogue, mental health management, productivity, communication",
+  robots: "index, follow",
+  openGraph: {
+    title: "Internal dialogue: Your AI-Powered Ai with Avatar Assistant",
+    description:
+      "Streamline your communication with Internal dialogue, the AI assistant that organizes your communication and enhances productivity.",
+    url: process.env.NEXT_PUBLIC_BASE_URL,
+    width: 1000,
+    height: 600,
+    type: "website",
+    images: "https://i.postimg.cc/wB2VKYqx/logo-1.png",
+  },
 };
 
 export default function RootLayout({
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,26 @@
 // app\page.tsx
 import { HomeScreen } from "@/components/HomeScreen";
 
+export const metadata = {
+  title:
+    "Internal dialogue: Your AI-Powered internal dialogue and avatar management for Efficient Communication",
+  description:
+    "Internal dialogue is your AI-powered ai assistant that streamlines communication, organizes your stress, and boosts productivity!",
+  keywords:
+    "AI Avatar assistant, Internal dialogue, mental health management, productivity, communication",
+  robots: "index, follow",
+  openGraph: {
+    title: "Internal dialogue: Your AI-Powered Ai with Avatar Assistant",
+    description:
+      "Streamline your communication with Internal dialogue, the AI assistant that organizes your communication and enhances productivity.",
+    url: process.env.NEXT_PUBLIC_BASE_URL,
+    width: 1000,
+    height: 600,
+    type: "website",
+    images:
+      "https://i.postimg.cc/wB2VKYqx/logo-1.png",
+  },
+};
 export default function HomePage() {
   return <HomeScreen />;
 }
